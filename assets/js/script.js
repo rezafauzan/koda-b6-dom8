07f89761ba8b5df986b575df544a3d4fc2022c20
rezafauzan593@gmail.com <rezafauzan593@gmail.com>
2026-01-19 10:29:24 +0700

    Feat: add feature to save convert result to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ function elementor(element, attribute = [['', '']], content, parrent) {
         e.appendChild(textNode)
     }
 
-    if(parrent){
+    if (parrent) {
         parrent.appendChild(e)
     }
     return e
@@ -22,28 +22,62 @@ const celciusConverterForm = document.getElementById('celcius-converter-form')
 const tbodyHistory = document.querySelector("#convert-history tbody")
 
 let index = 0
-celciusConverterForm.addEventListener('submit', 
+
+let histories = JSON.parse(window.localStorage.getItem('convert-histories'))
+if (histories === null) {
+    histories = []
+} else {
+    histories.forEach(
+        history => {
+            const trow = elementor('tr', [], undefined, undefined)
+            const tdIndex = elementor('td', [], index + 1, trow)
+            const tdCelcius = elementor('td', [], `${history.celcius}°`, trow)
+            const tdFahrenheit = elementor('td', [], `${history.fahrenheit}°`, trow)
+            const tdReamur = elementor('td', [], `${history.reamur}°`, undefined)
+            const tdKelvin = elementor('td', [], `${history.kelvin}°`, undefined)
+        
+            trow.append(tdIndex, tdCelcius, tdFahrenheit, tdReamur, tdKelvin)
+        
+            tbodyHistory.appendChild(trow)
+        
+            index++
+        }
+    )
+}
+
+
+
+celciusConverterForm.addEventListener('submit',
     e => {
         e.preventDefault()
+
         const temp = parseInt(new FormData(e.target).get('temp'))
-        if(typeof temp === 'number' && !isNaN(temp)){
-            const fahrenheit = (temp * 9/5) + 32
-            const reamur = temp * 4/5
+        if (typeof temp === 'number' && !isNaN(temp)) {
+            const fahrenheit = (temp * 9 / 5) + 32
+            const reamur = temp * 4 / 5
             const kelvin = temp + 273.15
-    
+            histories.push(
+                {
+                    celcius: temp,
+                    fahrenheit: fahrenheit,
+                    reamur: reamur,
+                    kelvin: kelvin
+                }
+            )
+            window.localStorage.setItem('convert-histories', JSON.stringify(histories))
             const trow = elementor('tr', [], undefined, undefined)
-            const tdIndex = elementor('td', [], index+1, trow)
+            const tdIndex = elementor('td', [], index + 1, trow)
             const tdCelcius = elementor('td', [], `${temp}°`, trow)
             const tdFahrenheit = elementor('td', [], `${fahrenheit}°`, trow)
             const tdReamur = elementor('td', [], `${reamur}°`, undefined)
             const tdKelvin = elementor('td', [], `${kelvin}°`, undefined)
-    
+
             trow.append(tdIndex, tdCelcius, tdFahrenheit, tdReamur, tdKelvin)
-    
+
             tbodyHistory.appendChild(trow)
-            
+
             index++
-        }else{
+        } else {
             alert("Input suhu harus berupa angka")
         }
     }
